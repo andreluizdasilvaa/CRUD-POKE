0e98bc9c2133ef64a5358a0962a48c8f2bf13977
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2024-10-14 21:24:49 -0300

    add++

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
+const path = require('path');
+
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // permite o express entenda dados de formulario html
+app.use(cors());
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=- | POST | =-=-=-=-=-=-=-=-=-=-=-=-=-
-
-
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=- | GET | =-=-=-=-=-=-=-=-=-=-=-=-=-
+app.get('/pokefeed', async (req, res)=> {
+    try {
+        const poke = await prisma.pokemon.findMany();
+
+        res.status(201).json(poke);
+        
+    } catch (error) {
+        console.error('ERROR: ', error);
+    }
+})
+
+// rota get exibir os pkemons em json + tratar no front
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=- | POST | =-=-=-=-=-=-=-=-=-=-=-=-=-
+app.post('/register', async (req, res)=> {
+    try {
+        const { nome, tipo, url_img } = req.body;
+
+        if (!nome || !tipo || !url_img) {
+            res.json({ msg: "Dados faltantes" }).end();
+        };
+
+        const pokemon = await prisma.pokemon.create({
+            data: {
+                name: nome,
+                tipo: tipo,
+                url_image: url_img,
+            },
+        });
+        res.status(201).json({ msg: "Pokeon registrado com sucesso!"});
+    } catch (error) {
+        console.error('ERROR: ', error);
+    };
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=> {
--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,5 +1,3 @@
-const { json } = require("express");
-
 document.addEventListener('DOMContentLoaded', ()=> {
     const modal_form = document.getElementById('modal_form');
     const form = document.getElementById('form_add');
@@ -9,16 +7,82 @@ document.addEventListener('DOMContentLoaded', ()=> {
     button_exit_modal.addEventListener('click', ()=> {
         modal_form.style.display = 'none'
     })
-    // abre modal com add
+    
+    // MOSTRAR TODOS POKEMONS
+    fetch("http://localhost:5000/pokefeed",{
+        method: 'GET'
+    }).then((response)=> response.json())
+    .then((item) => {
+        const container_poke = document.getElementById("container_poke");
+        container_poke.innerHTML = '';
+
+        item.forEach((poke)=> {
+            const cardPoke = document.createElement('div');
+            const classesToAdd = `flex gap-2 bg-black p-5 rounded-xl border-2 border-white min-w-[500px]`;
+            cardPoke.classList.add(...classesToAdd.split(" "));
+
+
+            cardPoke.innerHTML = `
+            <div class="bg-gradient-to-r from-white to-[#7B00FF] p-1 rounded-xl">
+                    <div class="bg-white rounded-xl">
+                        <img class="w-[400px] h-[400px] rounded-xl"
+                            src="${poke.url_image}" alt="">
+                    </div>
+
+                </div>
+
+                <div class="flex flex-col min-w-96 text-center justify-center items-center gap-4">
+                    <h1 class="text-6xl">${poke.name}</h1>
+                    <p class="text-xl">Tipo: ${poke.tipo}</p>
+                    <button id="button_delete"
+                        class="text-black bg-white rounded-md px-3 py-2 shadow-custom text-2xl hover:bg-[#7B00FF] hover:text-white hover:shadow-customtwo transition-all min-w-48">Deletar</button>
+                    <button id="button_atualizar"
+                        class="text-black bg-white rounded-md px-3 py-2 shadow-custom text-2xl hover:bg-[#7B00FF] hover:text-white hover:shadow-customtwo transition-all min-w-48">Atualizar</button>
+                </div>
+            `;
+            container_poke.appendChild(cardPoke);
+        })
+    })
+
+
+    // abre modal com botão adicionar
     const button_adicionar = document.getElementById('button_adicionar');
     button_adicionar.addEventListener('click', ()=> {
         modal_form.style.display = 'flex'
-    })
-
-    const button_submit = document.getElementById('button_submit');
-    button_submit.addEventListener('click', (event)=> {
-        event.preventDefault();
         
-        // pegar todos dados do form e cadastrar
+        // ADICIONAR POKEMON
+        const button_submit = document.getElementById("button_submit");
+        button_submit.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const nome_poke = document.getElementById("nome_poke").value;
+            const tipo_poke = document.getElementById("tipo_poke").value;
+            const url_img = document.getElementById("url_img").value;
+
+            if(!nome_poke || !tipo_poke || !url_img) {
+                alert('Prencha todos os campos do formulario');
+                return; // para a leitura do code
+            } else {
+                // enviar dados do form
+                fetch("http://localhost:5000/register", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        nome: nome_poke,
+                        tipo: tipo_poke,
+                        url_img: url_img,
+                    }),
+                }).then((response) => {
+                    if (response.ok) {
+                        window.location.reload();
+                    }
+                });
+            }
+
+
+            
+        });
     })
 });
